86e35d945557d1064396d406d99362c9f0fbfc59
Si Chen <chensi_0111@me.com>
2017-08-12 21:37:47 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ProfileComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule
